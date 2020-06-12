96829d22435481e824fcff04ed1d35f4887ce45b
unknown <macjay314@gmail.com>
2020-06-12 10:43:15 -0700

    Add ParkController

---
+++ ParksApi/Controllers/ParksController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ParksApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ParksApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private ParksApiContext _db;
+
+    public ParksController(ParksApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/Parks
+    [HttpGet]
+    public ActionResult<IEnumerable<Park>> Get()
+    {
+      return _db.Parks.ToList();
+    }
+
+    // POST api/Parks
+    [HttpPost]
+    public void Post([FromBody] Park park)
+    {
+      _db.Parks.Add(park);
+      _db.SaveChanges();
+    }
+
+    // GET api/parks/#
+    [HttpGet("{id}")]
+    public ActionResult<Park> Get(int id)
+    {
+      return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    }
+
+    //PUT api/parks/userId/parkId
+    // [HttpPut("{userId}/{id}")]
+    // public void Put(int userId, int id, [FromBody] Park park)
+    // {
+    //   park.ParkId = id;
+    //   if (park.UserId == userId)
+    //   {
+    //     _db.Entry(park).State = EntityState.Modified;
+    //     _db.SaveChanges();
+    //   }
+    // }
+
+    //http://localhost:5000/api/parks/1/9
+    // [HttpDelete("{userId}/{id}")]
+    // public void Delete(int id, int userId)
+    // {
+    //   var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    //   if (parkToDelete.UserId == userId)
+    //   {
+    //     _db.Parks.Remove(parkToDelete);
+    //     _db.SaveChanges();
+    //   }
+    // }
+
+    // // GET api/Parks
+    // [HttpGet]
+    // // public ActionResult<IEnumerable<Park>> Get(string name, string type, string rating, string category, string description, int userId)
+    // public ActionResult<Dictionary<string, object>> Get(string name, string type, string rating, string category, string description, int userId)
+    // {
+    //   var query = _db.Parks.AsQueryable();
+
+    //   if (name != null)
+    //   {
+    //     query = query.Where(entry => entry.Name == name);
+    //   }
+
+    //   if (type != null)
+    //   {
+    //     query = query.Where(entry => entry.Type.Contains(type));
+    //   }
+
+    //   if (description != null)
+    //   {
+    //     query = query.Where(entry => entry.Description.Contains(description));
+    //   }
+
+    //   if (rating != null)
+    //   {
+    //     query = query.Where(entry => entry.Rating == rating);
+    //   }
+
+    // //   if (userId != 0)
+    // //   {
+    // //     query = query.Where(entry => entry.UserId == userId);
+    // //   }
+    //   Dictionary<string, object> response = new Dictionary<string, object>();
+    //   response.Add("parks", query);
+    //   return response;
+    // }
+  }
+}
---
+++ ParksApi/Migrations/20200612173102_Initial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Type = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: false),
+                    Rating = table.Column<int>(nullable: false),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "ImageUrl", "Name", "Rating", "Type" },
+                values: new object[] { 1, "Marine camping ground", null, "Fort Casey", 3, "State" });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "ImageUrl", "Name", "Rating", "Type" },
+                values: new object[] { 2, "Shoreline on Port Townsend Bay", null, "Old Fort Townsend", 4, "State" });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Description", "ImageUrl", "Name", "Rating", "Type" },
+                values: new object[] { 3, "Snow covered mountains, lush rain forests & dramatic Pacific coastline", null, "Olympic", 5, "National" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
